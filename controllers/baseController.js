f58e80f10d9a6344c7c74897d95de9a7257be12f
sarahandressa <sarahagdiaz@yahoo.com.br>
2025-06-08 19:49:19 -0600

    Learning Activity W04 - Sessions and Messages

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,6 +2,7 @@ const utilities = require("../utilities")
 const baseController = {}
 
 baseController.buildHome = async function(req, res){
+  req.flash("notice", "This is a flash message.")
   const nav = await utilities.getNav()
   res.render("index", {title: "Home", nav})
 }
